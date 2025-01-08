85421b36adfa907fadfa1bc3968a79a0c0a8b3a4
Syed Sibteali Baqar <sibteali786@gmail.com>
2025-01-08 10:00:13 +0500

    Add initial API implementation with healthcheck endpoint

---
+++ cmd/api/healthcheck.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// Declare a handler which writes a plain-text response with information about the
+// application status, operating environment and version.
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "status: available")
+	fmt.Fprintf(w, "environment: %v\n", app.config.env)
+	fmt.Fprintf(w, "version: %v\n", version)
+
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module greenlight.sibteali.net
+
+go 1.23.1
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
