db1eb8a25d334b095a2163f64bcd59624ca438e4
Syed Sibteali Baqar <sibteali786@gmail.com>
2025-01-11 22:21:43 +0500

    Update Movie struct JSON tags for optional fields and exclude CreatedAt

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,10 +4,10 @@ import "time"
 
 type Movie struct {
 	ID        int64     `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
+	CreatedAt time.Time `json:"-"`
 	Title     string    `json:"title"`
-	Year      int32     `json:"year"`
-	Runtime   int32     `json:"runtime"`
-	Genres    []string  `json:"genres"`
+	Year      int32     `json:"year,omitempty"`           // force it to be string
+	Runtime   int32     `json:"runtime,omitempty,string"` // chose to parse field as a string
+	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"`
 }
