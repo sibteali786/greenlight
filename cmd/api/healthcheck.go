539efd56a0222d3a6f906da28225463e8c3cfb9c
Syed Sibteali Baqar <sibteali786@gmail.com>
2025-01-12 17:35:47 +0500

    Implement custom error handling responses for server errors and not found errors

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -16,6 +16,6 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 	// err := app.writeJSONViaEncoder(w, http.StatusOK, data, nil)
 	if err != nil {
 		app.logger.Println(err)
-		http.Error(w, "The server encuntered a problem and could not process your request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 }
