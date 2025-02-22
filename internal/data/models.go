bc0388d6156974ff15b12e6ff513fe193c9edc4f
Syed Sibteali Baqar <sibteali786@gmail.com>
2025-02-22 21:10:15 +0500

    Add models package with custom error and movie model initialization

---
+++ internal/data/models.go
@@ -0,0 +1,22 @@
+package data
+
+import (
+	"database/sql"
+	"errors"
+)
+
+// Define a custom ErrRecordNotFound error. We'll return this from our Get() method when
+// looking up a movie that doesn't exist in our database.
+var (
+	ErrorRecordNotFound = errors.New("record not found")
+)
+
+type Models struct {
+	Movies MovieModel
+}
+
+func NewModels(db *sql.DB) Models {
+	return Models{
+		Movies: MovieModel{DB: db},
+	}
+}
